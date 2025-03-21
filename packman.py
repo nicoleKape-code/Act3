67cdc413836cea1974af7bff9c6e0f8425acc042
nicoleKape-code <nikikapekape@gmail.com>
2025-03-20 21:24:15 -0600

    Funcionalidad cambiar posición random de pacman

--- packman.py
+++ packman.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, randint
 from turtle import *
 from freegames import floor, vector
 
@@ -6,7 +6,7 @@ state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
-pacman = vector(-40, -80)
+pacman = vector(randint(-9,-9) * 20, randint(-9, 9) * 20)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
